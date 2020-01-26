d4d2e4bb5bb8427afcd244f955e48bf63198368a
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-01-26 11:03:52 -0500

    eleventy config setTemplate formats - copies images/css files to dist folder - Minifies css - Copy images folder

--- apps/.eleventy.js
+++ apps/.eleventy.js
@@ -6,6 +6,20 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "../projects/**/styles/**/*.css": "styles"
   });
+  // eleventyConfig.setTemplateFormats([
+  //   "jpg",
+  //   "png",
+  //   "svg",
+  //   "css" // css is not yet a recognized template extension in Eleventy
+  // ]);
+
+  eleventyConfig.addPassthroughCopy({
+    "../projects/**/styles/**/*.css": "styles"
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    "../projects/**/images/": "images"
+  });
 
   return {
     dir: {
