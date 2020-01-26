5fafb5a5ece72c61e7151ab535865af7d28d3a58
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-01-26 11:05:24 -0500

    Minifies CSS on css output - generates reports for min/non min files

--- projects/shawnsandy/gulpfile.js
+++ projects/shawnsandy/gulpfile.js
@@ -1,9 +1,11 @@
 "use strict";
 
-var Fiber = require("fibers");
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+const Fiber = require("fibers");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 const reports = require("gulp-sizereport");
+const rename = require("gulp-rename");
+const minify = require("gulp-clean-css");
 sass.compiler = require("dart-sass");
 
 gulp.task("sass", function() {
@@ -11,7 +13,15 @@ gulp.task("sass", function() {
     .src("./sass/**/*.scss")
     .pipe(sass({ fiber: Fiber }).on("error", sass.logError))
     .pipe(gulp.dest("./styles"))
-    .pipe(reports({ gzip: true }));
+    .pipe(reports({ gzip: true }))
+    .pipe(minify())
+    .pipe(
+      rename(function(path) {
+        path.extname = ".min.css";
+      })
+    )
+    .pipe(reports({ gzip: true }))
+    .pipe(gulp.dest("./styles"));
 });
 
 gulp.task("sass:watch", function() {
