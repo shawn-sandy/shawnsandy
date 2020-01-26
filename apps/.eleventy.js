a98e427a5c8820f5c383e08dcf607ac48160ba38
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-01-26 11:31:39 -0500

    implements styling via tokes - integrates @shawnsandy/mix - Add gulp task for compling sass/css - Updates eleventy to copy assets (image/css)

--- apps/.eleventy.js
+++ apps/.eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy({
-    "../projects/**/images/": "images"
+    "../projects/**/images/**.*": "images"
   });
 
   return {
