1c803d1210aa9573f9ba12118709e5bee2376099
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-03-24 19:06:20 -0400

    change direction down/up

--- projects/shawnsandy/js/anime.js
+++ projects/shawnsandy/js/anime.js
@@ -1,7 +1,9 @@
 var animation = anime({
   targets: "#logo-svg",
-  translateY: 50,
-  duration: 1000,
+  delay: 1000,
+  translateY: -50,
+  opacity: 0.05,
+  duration: 800,
   easing: "linear",
   direction: "alternate"
 });
