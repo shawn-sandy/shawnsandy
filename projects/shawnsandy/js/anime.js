acdaa81e929e0e99a49f23fb2fbbf57f4c62f04b
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-03-25 04:15:32 -0400

    adds intro text

--- projects/shawnsandy/js/anime.js
+++ projects/shawnsandy/js/anime.js
@@ -1,7 +1,7 @@
 var animation = anime({
-  targets: "#logo-svg",
+  targets: "#lead-text",
   delay: 1000,
-  translateY: -50,
+  // translateY: -50,
   opacity: 0.05,
   duration: 800,
   easing: "linear",
